3c7255c75bd7405eca0b46c55e6bdaac2d98eb78
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-05-28 10:03:48 +0530

    Javascript tasks

---
+++ Task5.js
@@ -0,0 +1,7 @@
+//Write a program that uses a loop to calculate and display the sum of all numbers from 1 to 10.
+let sum = 0;
+for(let i = 1 ; i<=10 ; i++)
+    {
+        sum += i;
+    }
+console.log(`1+2+3+....+10=${sum}`);
---
+++ Task6.js
@@ -0,0 +1,7 @@
+let givenNumber = 5;
+let factorial = 1;
+for(let i=givenNumber ; i>=1 ; i--)
+    {
+        factorial *= i;
+    }
+console.log(`The factorial of ${givenNumber} is ${factorial}`);
