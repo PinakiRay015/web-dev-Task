5968472e5e227efce7a594ba1ff7bf2e567e9e2c
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-05-29 08:51:06 +0530

    Task 12

---
+++ web-dev-starter/Task12.js
@@ -0,0 +1,22 @@
+let users = [
+  { id: 1, name: "A", age: 25 },
+  { id: 2, name: "B", age: 30 },
+  { id: 3, name: "C", age: 22 },
+];
+
+const userIdToUpdate = 2;
+let isFound = false;
+
+for (let key in users) {
+  if (userIdToUpdate == users[key].id) {
+    users[key].age = 35;
+    console.log(`The age of user havind id ${userIdToUpdate} is updated!!`);
+    console.log("Here is your updated array object");
+    console.log(users[key]);
+    isFound = true;
+    break;
+  }
+}
+if (!isFound) {
+  console.log("User is not found");
+}
