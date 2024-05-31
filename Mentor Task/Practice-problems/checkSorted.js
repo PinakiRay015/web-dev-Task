98100847e0c69c5d62e1fc01e1edf553aa2ea338
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-05-31 08:25:03 +0530

    check an array is sorted or not

--- Mentor Task/Practice-problems/checkSorted.js
+++ Mentor Task/Practice-problems/checkSorted.js
@@ -1,17 +1,36 @@
-const array1 = [34, 22 , 11 , 18, 45, 7];
-const array2 = [10, 20 , 30 , 40 , 50];
-let isSorted1 = true;
+const array1 = [34, 22, 11, 18, 45, 7];
+const array2 = [10, 20, 30, 40, 50];
 
 //using for loop
-const checkSort = (array) =>{
-    for(let i = 0 ; i<array.length ; i++){
-        if(array[i] > array[i+1]){
-            isSorted1 = false;
-            return;
-        }
+const checkSort1 = (array) => {
+  let isSorted = true;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      isSorted = false;
+      return;
     }
+  }
 
-    return (!isSorted1) ? false : true;
-}
+  return !isSorted ? false : true;
+};
 
-console.log((checkSort(array2)) ? 'Array is sorted' : 'Array is not sorted');
+//using while loop
+const checkSort2 = (array) => {
+  let isSorted = true;
+  let i = 0;
+  while (i < array.length) {
+    if (array[i] > array[i + 1]) {
+      isSorted = false;
+      return;
+    }
+    i++;
+  }
+
+  return !isSorted ? false : true;
+};
+
+console.log(checkSort1(array1) ? "Array is sorted" : "Array is not sorted");
+console.log(checkSort1(array2) ? "Array is sorted" : "Array is not sorted");
+console.log("============")
+console.log((checkSort2(array2)) ? 'Array is sorted' : 'Array is not sorted');
+console.log((checkSort2(array1)) ? 'Array is sorted' : 'Array is not sorted');
