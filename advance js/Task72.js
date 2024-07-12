e5e70781e494a78f1d886611416ce94cf4515700
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-07-12 07:58:33 +0530

    task 72

---
+++ advance js/Task72.js
@@ -0,0 +1,11 @@
+const person = 
+    {
+        id : 1,
+        name : "Pinaki",
+        address : "Hyderbad",
+        age : 23
+    }
+
+const {name , age} = person;
+
+console.log(name , age);
